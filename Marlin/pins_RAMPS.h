74f10b65df5447f1e0c7a8aa19fe8f30b538282b
Riley Larche <riley.larche@gmail.com>
2019-01-15 17:12:13 -0800

    Updated bed thermistor pin used
    T1 bad connection, T2 working.
    
    Printer optimization undone, will have to update

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -168,8 +168,8 @@
 // Temperature Sensors
 //
 #define TEMP_0_PIN         13   // Analog Input
-#define TEMP_1_PIN         15   // Analog Input
-#define TEMP_BED_PIN       14   // Analog Input
+#define TEMP_1_PIN         14   // Analog Input
+#define TEMP_BED_PIN       15   // Analog Input
 
 // SPI for Max6675 or Max31855 Thermocouple
 #if DISABLED(SDSUPPORT)
